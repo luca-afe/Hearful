f263d9a7f3c1415dbce168e444de933fd6521013
Luca Afeltra <problast666@gmail.com>
2026-09-23 16:12:30 +0200

    revert minor changes for testing purposes

--- hearful-app/src/pages/Home.tsx
+++ hearful-app/src/pages/Home.tsx
@@ -57,7 +57,7 @@ export default function Home() {
                         Subtitled.
                     </h1>
                     <p className="text-gray-300 text-sm max-w-[280px] font-light">
-                        Ascolta pippo luigi ogni attimo visualizzando trascrizioni in tempo reale di ciò che ti circonda, direttamente sui tuoi occhiali AR.
+                        Ascolta ogni attimo visualizzando trascrizioni in tempo reale di ciò che ti circonda, direttamente sui tuoi occhiali AR.
                     </p>
                 </div>
 
